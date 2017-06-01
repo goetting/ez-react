c71438b59deee103bfb87b067ce0e1e258f7f80e
Goetz Goetting <goetz.goetting@web.de>
2017-06-01 11:51:43 +0200

    Changed: finished untested/unclean rewrite

--- src/index.js
+++ src/index.js
@@ -2,74 +2,58 @@
 import React from 'react';
 import EZFlux from 'ez-flux';
 
-type StateHandler = (state: Object, props: any) => Object | void;
+type Store = Object;
+type Stores = { [storeName: string]: Store };
+type StateHandler = (Store, props: any) => Object | void;
+type Handler = StateHandler | string[];
+type Handlers = { [storeName: string]: Handler };
 
-const badHandlersMsg = '3rd arg must be an object mapping state keys to eventHandlers';
-const badComponentMsg = '2nd arg must be a React Component class or instance';
-const getBadStateMsg = (name: string): string => `key "${name}" not found on state of ezFlux instance`;
+const handlerErr = '2nd arg must be an object mapping state keys to eventHandlers';
 
-const isFn = fn => typeof fn === 'function';
+function normalise(handler: Handler): StateHandler {
+  if (typeof handler === 'function') return handler;
+  if (Array.isArray(handler)) return store => handler.reduce((acc, k) => acc[k] = store[k], {});
+  throw new Error(handlerErr);
+}
 
-export default function createConnector(stores) =>
-  const validateArguments = (component: any, handlers: Handlers): void => {
-    if (!isFn(component)) throw new Error(badComponentMsg);
-    if (handlers && typeof handlers === 'object') throw new Error(badHandlersMsg);
+export default function createConnector(stores: Stores): Function {
+  return (Component: Function, handlers: Handlers, initProps: Object = {}): Function =>
+    class EZWrapper extends React.PureComponent {
+      isMounted: boolean = true;
+      events: { name: string, fn: () => void }[];
+      state: Object = initProps;
+      props: Object;
 
-    const names: string[] = Object.keys(handlers);
+      componentDidMount() {
+        if (typeof Component !== 'function') throw new Error('1st arg must be a React Component');
+        if (typeof handlers !== 'object') throw new Error(handlerErr);
 
-    for (let i = names.length; i--;) {
-      const name = names[i];
-      if (!isFn(handlers[name]) && !Array.isArray(handlers[name])) throw new Error(badHandlersMsg);
-      if (!stores[name]) throw new Error(getBadStateMsg(name));
-    }
-  };
+        this.events = Object
+          .keys(handlers)
+          .map((name) => {
+            if (!stores[name]) throw new Error(`store "${name}" unknown to this connector`);
 
-  const createHandler = (keys): StateHandler =>
-    (store) => {
-      const newState = {};
-      for (let i = keys.length; i--;) newState[keys[i]] = store[keys[i]];
-      return newState;
+            const stateHandler: StateHandler = normalise(handlers[name]);
+            const store: Store = stores[name];
+            const fn = () => {
+              if (!this.isMounted) return;
+              const newState: any = stateHandler(store, this.state);
+
+              if (newState) this.setState(newState);
+            };
+
+            store.$on('change', fn);
+            return { name, fn };
+          });
+      }
+
+      componentWillUnmount() {
+        this.isMounted = false;
+        this.events.forEach(e => stores[e.name].$off('change', e.fn));
+      }
+
+      render() {
+        return <Component { ...this.props } { ...this.state } />;
+      }
     };
-
-  const addListeners = (instance: Object, handlers: Handlers): Function[] =>
-    Object
-      .keys(handlers)
-      .map((name) => {
-        const stateHandler = isFn(handlers[name]) ? handlers[name] : createHandler(handlers[name]);
-        const store = stores[name];
-        const fn = () => {
-          if (instance.willUnmount) return;
-          const newState: any = stateHandler(store, instance.state);
-
-          if (newState) instance.setState(newState);
-        };
-        store.$on('change', fn);
-        return () => store.$off('change', fn);
-      });
-
-  return function connect(Component: Function, handlers: Handlers, initProps: Object): Function => {
-      validateArguments(Component, handlers);
-
-      return class EZWrapper extends React.Component {
-        eventRemovers: Function[] = [];
-        willUnmount: boolean = false;
-        state: Object = initProps || {};
-        props: Object;
-
-        componentDidMount() {
-          this.eventRemovers = addListeners(this, handlers);
-        }
-
-        componentWillUnmount() {
-          this.willUnmount = true;
-          this.eventRemovers.forEach(fn => fn());
-        }
-
-        render() {
-          const childProps = Object.assign({}, this.props, this.state);
-
-          return React.createElement(Component, childProps, this.props.children);
-        }
-      };
-    },
 }
